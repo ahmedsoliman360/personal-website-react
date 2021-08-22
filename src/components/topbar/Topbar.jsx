3d21c8bb98db96a4e3797e63dfe89fe3ef19ca8c
Ahmed Soliman <ahmedsoliman360@gmail.com>
2021-08-22 13:42:26 +0400

    Added Content and styling to the components of the topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./Topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 export default function Topbar() {
   return (
     <div className="topbar" id="topbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">Nav to Intro</a>
+          <a href="#intro" className="logo">
+            SOLI.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+971561415142</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>ahmedsoliman360@gmail.com</span>
+          </div>
         </div>
-        <div className="right"></div>
+
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
