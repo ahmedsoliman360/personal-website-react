9371eae941d7a92d6adc1d88cee2474bfdd6677f
Ahmed Soliman <ahmedsoliman360@gmail.com>
2021-08-23 17:43:42 +0400

    Halfway through portfolio yet to implement active class transition

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,46 @@
 import React from "react";
 import "./Portfolio.scss";
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Websites</li>
+        <li>Java Projects</li>
+        <li>Data Science</li>
+        <li>Python Projects</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://images.unsplash.com/photo-1566241477600-ac026ad43874?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1030&q=80"
+            alt=""
+          />
+          <h3>Test App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://images.unsplash.com/photo-1566241477600-ac026ad43874?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1030&q=80"
+            alt=""
+          />
+          <h3>Test App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://images.unsplash.com/photo-1566241477600-ac026ad43874?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1030&q=80"
+            alt=""
+          />
+          <h3>Test App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://images.unsplash.com/photo-1566241477600-ac026ad43874?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1030&q=80"
+            alt=""
+          />
+          <h3>Test App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
